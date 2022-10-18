365e357a8aba4e58f7c388818e22027628c945c8
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-10-18 14:00:12 +0200

    style: Box massage

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -23,6 +23,47 @@ const Form = () => {
                         <input className='Envoye' type="submit" value="Envoyer" />
                     </div>
                 </form>
+
+                <section>
+
+                    <main class="msger-chat">
+                        <div class="msg left-msg">
+                            <div
+                                class="msg-img"
+                                
+                            ></div>
+
+                            <div class="msg-bubble">
+                                <div class="msg-info">
+                                    <div class="msg-info-name">BOT</div>
+                                    <div class="msg-info-time">12:45</div>
+                                </div>
+
+                                <div class="msg-text">
+                                    Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="msg right-msg">
+                            <div
+                                class="msg-img"
+                                
+                            ></div>
+
+                            <div class="msg-bubble">
+                                <div class="msg-info">
+                                    <div class="msg-info-name">Sajad</div>
+                                    <div class="msg-info-time">12:46</div>
+                                </div>
+
+                                <div class="msg-text">
+                                    You can change your name in JS section!
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </section>
             </div >
         </>
     )
